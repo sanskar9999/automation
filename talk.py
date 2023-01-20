7439a1ac5c6a1a613ced4cc57d2b7e6d41353261
npc <115063350+sanskar9999@users.noreply.github.com>
2023-01-20 21:17:38 +0530

    Update talk.py

--- talk.py
+++ talk.py
@@ -3,7 +3,7 @@ import pyautogui as pe
 import time
 import subprocess
 
-# Start Brave browser
+# Start Brave browser (replace this with the directory of any browser you use)
 os.startfile('C:/Program Files/BraveSoftware/Brave-Browser/Application/brave.exe')
 time.sleep(1)
 
@@ -22,26 +22,26 @@ pe.typewrite('https://discord.com/channels/828151284233535488/105606588959162372
 time.sleep(0.1)
 pe.press('enter')
 
-# Move cursor to a specific location (the text above)
+# Move cursor to a specific location (the previous reply)
 pe.moveTo(581,858,0.1)
 
-# Perform a loop for 10 iterations
+# Perform a loop for 10 iterations, edit to edit how many time you want the two bots to message each other
 for i in range(10): 
     # Click, copy, switch tabs, paste, and press enter
-    pe.click(),pe.click(),pe.click(),
+    pe.click(),pe.click(),pe.click(), # Triple click To select the whole reply
     time.sleep(0.1),
-    pe.hotkey('ctrl','c'),
+    pe.hotkey('ctrl','c'), # Copy the reply
     time.sleep(0.7),
     pe.click(),
     time.sleep(0.1),
-    pe.hotkey('ctrl','tab'),
+    pe.hotkey('ctrl','tab'), # Switch to other channel
     time.sleep(0.2),
     pe.click(),
     time.sleep(0.6),
-    pe.hotkey('ctrl','v'),
+    pe.hotkey('ctrl','v'), # Paste the reply
     time.sleep(0.1),
-    pe.press('enter'),
-    time.sleep(6)
+    pe.press('enter'), # Send the reply
+    time.sleep(6) # Wait for the response and repeat
 
 # Type execution complete
 pe.typewrite('Execution complete', 0.2)
